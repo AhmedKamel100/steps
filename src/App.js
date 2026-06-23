27cf2c602408dac0a1d463f8aef7b01ac31cb02e
AhmedKamel100 <akmoselhy@gmail.com>
2026-06-23 18:59:47 +0300

    changing call back function

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
   const [step, setStep] = useState(1);
 
   function handleClose() {
-    setIsOpen(!isOpen);
+    setIsOpen((open) => !open);
   }
 
   return (
@@ -63,12 +63,12 @@ function StepActions({ step, setStep }) {
   };
   function handlePrevious() {
     if (step === 1) return;
-    setStep(step - 1);
+    setStep((s) => s - 1);
   }
 
   function handleNext() {
     if (step === 3) return;
-    setStep(step + 1);
+    setStep((s) => s + 1);
   }
 
   return (
